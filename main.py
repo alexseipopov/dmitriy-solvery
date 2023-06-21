98497beccb0958596f09701d9675d8585bd66b92
Alexsei Popov <popov2895@gmail.com>
2023-06-21 19:04:13 +0300

    16.06

--- main.py
+++ main.py
@@ -13,11 +13,30 @@ class User(db.Model):
     id = sa.Column(sa.Integer, primary_key=True)
     login = sa.Column(sa.Text, unique=True, nullable=False)
     password = sa.Column(sa.Text, nullable=False)
-    date_of_birth = sa.Column(sa.DateTime)
+
+
+class Card(db.Model):
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.Text, nullable=False)
+    description = sa.Column(sa.Text)
+    price = sa.Column(sa.Numeric, nullable=False)
+    is_active = sa.Column(sa.Boolean, default=True)
 
 
 with app.app_context():
     db.create_all()
+
+
+# CRUD - Create Read Update Delete
+def create_response(status="SUCCESS", code=0, description="OK", data=None):
+    if data is None:
+        data = {}
+    return {
+            "status": status,
+            "code": code,
+            "description": description,
+            "data": data
+        }
 
 
 @app.route("/", methods=["POST"])
@@ -47,14 +66,14 @@ def registration():
     login = request.form.get("login")
     passwd = request.form.get("password")
     if not login or not passwd:
-        return "No such data in request"
+        return create_response("FAILURE", 1,  "No such data in request"), 400
     check_login = User.query.filter_by(login=login).first()
     if check_login:
-        return "User already exist"
+        return create_response("FAILURE", 2, "User already exist"), 400
     user = User(login=login, password=passwd)
     db.session.add(user)
     db.session.commit()
-    return "OK"
+    return create_response(), 200
 
 
 @app.post("/auth")
@@ -68,6 +87,40 @@ def auth():
     if not record or record.password != passwd:
         return "login or password not match"
     return "OK"
+
+
+@app.post("/create_card")
+def create_card():
+    name = request.form.get("name")
+    description = request.form.get("description")
+    price = request.form.get("price")
+    if name is None or price is None:
+        return create_response("Failure", 1, "No such data in request"), 400
+    card = Card(name=name, price=price, description=description)
+    db.session.add(card)
+    db.session.commit()
+    return create_response(data={
+        "id": card.id,
+        "name": card.name,
+        "description": card.description,
+        "price": card.price,
+        "is_active": card.is_active
+    })
+
+
+@app.get("/get_cards")
+def get_cards():
+    cards = Card.query.all()
+    response_data = []
+    for card in cards:
+        response_data.append({
+            "id": card.id,
+            "name": card.name,
+            "description": card.description,
+            "price": card.price,
+            "is_active": card.is_active
+        })
+    return create_response(data=response_data)
 
 
 if __name__ == "__main__":
